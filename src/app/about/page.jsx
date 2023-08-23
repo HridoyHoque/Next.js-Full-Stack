651d4ed18dc913a1bcdd606add07e009155a84ff
HridoyHoque <hhridoy155@gmail.com>
2023-08-23 06:04:23 +0600

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <div>
+                About Page
+            </div>
+        </div>
+    );
+};
+
+export default page;
