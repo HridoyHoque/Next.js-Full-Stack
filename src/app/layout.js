75b0410f914401b5a6212a8a704fa48d780c1e96
HridoyHoque <hhridoy155@gmail.com>
2023-08-23 19:23:59 +0600

    Added footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import Navbar from '@/components/navbar/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Roboto, Poppins } from 'next/font/google'
 import Footer from '@/components/footer/Footer'
+// import { ThemeContext } from './context/ThemeContext'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+
+    
+      <Navbar />
         {children}
         <Footer />
         </body>
